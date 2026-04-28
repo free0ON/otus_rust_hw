dd7eb37633a054aec832834105248e36aff2a3e0
free0ON <8333184@gmail.com>
2026-04-28 20:16:42 +0300

    Otus Rust hw02 without example

--- smart_home/src/lib.rs
+++ smart_home/src/lib.rs
@@ -1,49 +1,53 @@
 // OTUS Rust hw02 0.2.0
 // smart_home library refactoring
+use std::any::Any;
 use std::fmt::{self, Display};
-pub struct Home<'a> {
-    id: usize,
+use std::collections::HashMap;
+pub struct Home {
+    id: String,
     name: String,
-    rooms: &'a mut [Room<'a>],
+    rooms: HashMap<String, Box<Room>>,
 }
 
-impl<'a> Home<'a> {
-    pub fn new(_id: usize, _name: &str, _rooms: &'a mut [Room<'a>]) -> Self {
+impl Home {
+    pub fn new(_id: &str, _name: &str, _rooms: HashMap<String, Box<Room>>) -> Self {
         Self {
-            id: _id,
+            id: _id.to_string(),
             name: _name.to_string(),
             rooms: _rooms,
         }
     }
-    pub fn get_room(&self, index: usize) -> &Room<'a> {
-        self.rooms
-            .get(index)
-            .unwrap_or_else(|| panic!("Room index is outbounded"))
-    }
+    pub fn get_room(&self, index: &str) -> Option<&Box<Room>> {
+        self.rooms.get(index) 
+   }
 
-    pub fn get_mut_room(&mut self, index: usize) -> &mut Room<'a> {
-        self.rooms
-            .get_mut(index)
-            .unwrap_or_else(|| panic!("Room index is outbouded"))
+    pub fn get_mut_room(&mut self, index: &str) -> Option<&mut Box<Room>> {
+        self.rooms.get_mut(index)
     }
 
     pub fn update(&mut self) {
-        for room in self.rooms.iter_mut() {
-            for device in room.devices.iter_mut() {
-                device.update();
-            }
+        for (_id, room) in self.rooms.iter_mut() {
+            room.update();               
         }
     }
 
     pub fn report(&self) {
         println!("{}", self);
     }
+
+    pub fn add_room(&mut self, id: &str, _room: Box<Room>) {
+        self.rooms.insert(id.to_string(), _room);
+    }
+
+    pub fn delete_room(&mut self, id: &str) {
+        self.rooms.remove(id);
+    }
 }
 
-impl<'a> Display for Home<'a> {
+impl Display for Home {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let sum_rooms = self.rooms.iter().fold(String::new(), |mut acc, x| {
-            acc.push_str(x.to_string().as_str());
+        let sum_rooms = self.rooms.iter().fold(String::new(), |mut acc, (_id, room)| {
+            acc.push_str(room.to_string().as_str());
             acc
         });
 
@@ -55,54 +59,60 @@ impl<'a> Display for Home<'a> {
     }
 }
 
-impl<'a> PartialEq for Home<'a> {
+impl PartialEq for Home {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id && self.name == other.name
     }
 }
 
-pub struct Room<'a> {
-    id: usize,
+pub struct Room {
+    id: String,
     name: String,
-    devices: &'a mut [Device],
+    devices: HashMap<String, Box<dyn Device>>,
 }
 
-impl<'a> Room<'a> {
-    pub fn new(_id: usize, _name: &str, _devices: &'a mut [Device]) -> Self {
+impl Room {
+    pub fn new(_id: &str, _name: &str, _devices: HashMap<String, Box<dyn Device>>) -> Self {
         Self {
-            id: _id,
+            id: _id.to_string(),
             name: _name.to_string(),
             devices: _devices,
         }
     }
 
-    pub fn get_device(&self, index: usize) -> &Device {
-        if index < self.devices.len() {
-            &self.devices[index]
-        } else {
-            panic!("Device index is outbounded");
+    pub fn get_device(&self, index: &str) -> Option<&Box<dyn Device>> {
+        self.devices.get(index)
+    }
+
+    pub fn get_mut_device(&mut self, index: &str) -> Option<&mut Box<dyn Device>> {
+        self.devices.get_mut(index)
+    }
+
+    pub fn update(&mut self) {
+        for (_id, device) in self.devices.iter_mut() {
+            device.update();
         }
     }
 
-    pub fn get_mut_device(&mut self, index: usize) -> &mut Device {
-        if index < self.devices.len() {
-            &mut self.devices[index]
-        } else {
-            panic!("Device index is outdounded")
-        }
+    pub fn add_device(&mut self, id: &str, _device: Box<dyn Device>) {
+        self.devices.insert(id.to_string(), _device);
+    }
+
+    pub fn delete_device(&mut self, id: &str) {
+        self.devices.remove(id);
     }
 }
 
-impl<'a> PartialEq for Room<'a> {
+impl PartialEq for Room {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id && self.name == other.name
     }
 }
 
-impl<'a> Display for Room<'a> {
+impl Display for Room {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let sum_devices = self.devices.iter().fold(String::new(), |mut acc, x| {
-            acc.push_str(x.to_string().as_str());
+        let sum_devices = self.devices.iter().fold(String::new(), |mut acc, (_id, device)| {
+            acc.push_str(device.to_string().as_str());
             acc
         });
 
@@ -114,66 +124,72 @@ impl<'a> Display for Room<'a> {
     }
 }
 
-#[derive(Debug)]
-pub enum Device {
-    ThermometerDevice(Thermometer),
-    SocketDevice(Socket),
-}
-
-impl Device {
-    pub fn update(&mut self) {
-        match self {
-            Device::ThermometerDevice(thermometer) => thermometer.update_temperature(),
-            Device::SocketDevice(socket) => socket.update_power(),
-        }
-    }
-
-    pub fn set_state(&mut self, state: SocketState) {
-        match self {
-            Device::ThermometerDevice(thermometer) => thermometer.update_temperature(),
-            Device::SocketDevice(socket) => {
-                socket.update_power();
-                socket.set_state(state);
-            }
-        }
-    }
-
-    pub fn get_state(&mut self) -> Option<SocketState> {
-        match self {
-            Device::ThermometerDevice(thermometer) => {
-                thermometer.update_temperature();
-                None
-            }
-            Device::SocketDevice(socket) => {
-                socket.update_power();
-                Some(socket.state)
-            }
-        }
-    }
-
-    pub fn get_power(&mut self) -> Option<f32> {
-        match self {
-            Device::ThermometerDevice(_) => None,
-            Device::SocketDevice(socket) => {
-                socket.update_power();
-                Some(socket.power)
-            }
+pub trait Device: Any + Display {
+    fn update(&mut self);
+    fn report(&self) -> String;
+    fn set_state(&mut self, state: SocketState);
+    fn get_state(&self) -> Option<SocketState>;
+    fn as_any(&self) -> &dyn Any;
+    fn dyn_eq(&self, other: &dyn Device) -> bool
+    where
+        Self: Sized + PartialEq + Any,
+    {
+        if let Some(other) = other.as_any().downcast_ref::<Self>() {
+            println!("self: {}", self.report());
+            println!("other: {}", other.report());
+            *self == *other
+        } else {
+            false
         }
     }
 }
 
-impl fmt::Display for Device {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Device::ThermometerDevice(thermometer) => writeln!(f, "{}", thermometer),
-            Device::SocketDevice(socket) => writeln!(f, "{}", socket),
-        }
+
+impl Device for Thermometer {
+    fn update(&mut self) {
+        self.update_temperature();
+    }
+
+    fn report(&self) -> String {
+        format!("id: {}, name: {} ", self.id, self.name)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn set_state(&mut self, _state: SocketState) {
+        
+    }
+    fn get_state(&self) -> Option<SocketState> {
+        None
+    }
+}
+
+impl Device for Socket {
+    fn update(&mut self) {
+        self.update_power();
+    }
+
+    fn report(&self) -> String {
+        format!("id: {}, name: {}", self.id, self.name)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn set_state(&mut self, _state: SocketState) {
+        self.set_state(_state);
+    }
+
+    fn get_state(&self) -> Option<SocketState> {
+        Some(self.state)
     }
 }
 
 #[derive(Debug)]
 pub struct Thermometer {
-    id: usize,
+    id: String,
     name: String,
     temperature: f32,
     min_temperature: f32,
@@ -181,9 +197,9 @@ pub struct Thermometer {
 }
 
 impl Thermometer {
-    pub fn new(_id: usize, _name: &str, _min_temperature: f32, _max_temperature: f32) -> Self {
+    pub fn new(_id: &str, _name: &str, _min_temperature: f32, _max_temperature: f32) -> Self {
         Self {
-            id: _id,
+            id: _id.to_string(),
             name: _name.to_string(),
             temperature: 0.0,
             min_temperature: _min_temperature,
@@ -205,9 +221,10 @@ impl Thermometer {
         self.name.as_str()
     }
 
-    pub fn get_id(&self) -> usize {
-        self.id
+    pub fn get_id(&self) -> &str {
+        self.id.as_str()
     }
+
 }
 
 impl PartialEq for Thermometer {
@@ -229,6 +246,18 @@ impl Display for Thermometer {
     }
 }
 
+// impl From<dyn Device> for Thermometer {
+//     fn from(other: &(dyn Device + 'static)) -> Self {
+//         Self{
+//             id: other.id.clone(),
+//             name: other.name.clone(),
+//             temperature: other.temperature,
+//             max_temperature: other.max_temperature,
+//             min_temperature: other.min_temperature,
+//         }
+//     }
+// }
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum SocketState {
     On,
@@ -237,7 +266,7 @@ pub enum SocketState {
 
 #[derive(Debug)]
 pub struct Socket {
-    id: usize,
+    id: String,
     name: String,
     state: SocketState,
     power: f32,
@@ -245,9 +274,9 @@ pub struct Socket {
 }
 
 impl Socket {
-    pub fn new(_id: usize, _name: &str, _state: SocketState, _max_power: f32) -> Self {
+    pub fn new(_id: &str, _name: &str, _state: SocketState, _max_power: f32) -> Self {
         Socket {
-            id: _id,
+            id: _id.to_string(),
             name: _name.to_string(),
             state: _state,
             power: 0.0,
@@ -260,8 +289,8 @@ impl Socket {
         self.update_power();
     }
 
-    pub fn get_state(&self) -> &SocketState {
-        &self.state
+    pub fn get_state(&self) -> Option<SocketState> {
+        Some(self.state)
     }
 
     pub fn get_power(&mut self) -> f32 {
@@ -280,8 +309,8 @@ impl Socket {
         self.name.as_str()
     }
 
-    pub fn get_id(&self) -> usize {
-        self.id
+    pub fn get_id(&self) -> &str {
+        self.id.as_str()
     }
 }
 
@@ -303,20 +332,20 @@ impl PartialEq for Socket {
 
 #[cfg(test)]
 mod tests {
-
+    use std::collections::HashMap;
     use crate::{Device, Home, Room, Socket, SocketState, Thermometer};
     #[test]
     fn thermometr_test() {
-        let mut term1 = Thermometer::new(1, "Virtual thermometer", -50.0, 50.0);
-        let term2 = Thermometer::new(2, "Virtual thermometer", -50.0, 50.0);
+        let mut term1 = Thermometer::new("1", "Virtual thermometer", -50.0, 50.0);
+        let term2 = Thermometer::new("2", "Virtual thermometer", -50.0, 50.0);
         term1.update_temperature();
         assert!(term1 != term2);
     }
 
     #[test]
     fn socket_test() {
-        let mut socket1 = Socket::new(1, "Virtual socket", SocketState::On, 1000.0);
-        let mut socket2 = Socket::new(2, "Virtual socket", SocketState::Off, 1000.0);
+        let mut socket1 = Socket::new("1", "Virtual socket", SocketState::On, 1000.0);
+        let mut socket2 = Socket::new("2", "Virtual socket", SocketState::Off, 1000.0);
         socket1.update_power();
         socket2.update_power();
         assert!(socket1 != socket2);
@@ -326,52 +355,55 @@ mod tests {
 
     #[test]
     fn room_test() {
-        let term1 = Thermometer::new(1, "Virtual thermometer", -50.0, 50.0);
-        let term2 = Thermometer::new(2, "Virtual thermometer", -50.0, 50.0);
-        let socket1 = Socket::new(1, "Virtual socket", SocketState::On, 1000.0);
-        let socket2 = Socket::new(2, "Virtual socket", SocketState::Off, 1000.0);
-        let mut devices = [
-            Device::ThermometerDevice(term1),
-            Device::ThermometerDevice(term2),
-            Device::SocketDevice(socket1),
-            Device::SocketDevice(socket2),
-        ];
-        let room1 = Room::new(1, "Kitchen", &mut devices);
+        let term1 = Thermometer::new("1", "Virtual thermometer", -50.0, 50.0);
+        let term2 = Thermometer::new("2", "Virtual thermometer", -50.0, 50.0);
+        let socket1 = Socket::new("3", "Virtual socket", SocketState::On, 1000.0);
+        let socket2 = Socket::new("4", "Virtual socket", SocketState::Off, 1000.0);
+        let devices  = HashMap::<String, Box<dyn Device>>::new();
+        let mut room1 = Room::new("1", "Kitchen", devices);
+        room1.add_device("1", Box::<Thermometer>::new(term1));
+        room1.add_device("2", Box::<Thermometer>::new(term2));
+        room1.add_device("3", Box::<Socket>::new(socket1));
+        room1.add_device("4", Box::<Socket>::new(socket2));
         println!("{}", room1);
     }
 
     #[test]
     fn home_test() {
-        let term1 = Thermometer::new(1, "Virtual thermometer", -50.0, 50.0);
-        let term2 = Thermometer::new(2, "Virtual thermometer", -50.0, 50.0);
+        let term1 = Thermometer::new("1", "Virtual thermometer", -50.0, 50.0);
+        let term2 = Thermometer::new("2", "Virtual thermometer", -50.0, 50.0);
 
-        let socket1 = Socket::new(1, "Virtual socket", SocketState::On, 1000.0);
-        let socket2 = Socket::new(2, "Virtual socket", SocketState::Off, 1000.0);
-
-        let mut devices1 = [
-            Device::ThermometerDevice(term1),
-            Device::ThermometerDevice(term2),
-            Device::SocketDevice(socket1),
-            Device::SocketDevice(socket2),
-        ];
-        let room1 = Room::new(1, "Kitchen", &mut devices1);
-
-        let term3 = Thermometer::new(3, "Virtual thermometer", -50.0, 50.0);
-        let term4 = Thermometer::new(4, "Virtual thermometer", -50.0, 50.0);
-        let socket3 = Socket::new(3, "Virtual socket", SocketState::On, 1000.0);
-        let socket4 = Socket::new(4, "Virtual socket", SocketState::Off, 1000.0);
-        let mut devices2 = [
-            Device::ThermometerDevice(term3),
-            Device::ThermometerDevice(term4),
-            Device::SocketDevice(socket3),
-            Device::SocketDevice(socket4),
-        ];
-        let room2 = Room::new(2, "Hall", &mut devices2);
-
-        let rooms = &mut [room1, room2];
-        let mut home = Home::new(1, "My home", rooms);
-        let room = home.get_mut_room(1);
-        let device = room.get_mut_device(2);
+        let socket1 = Socket::new("3", "Virtual socket", SocketState::On, 1000.0);
+        let socket2 = Socket::new("4", "Virtual socket", SocketState::Off, 1000.0);
+                 
+        let mut room1 = Room::new("1", "Kitchen", HashMap::<String, Box<dyn Device>>::new());
+        let term3 = Thermometer::new("5", "Virtual thermometer", -50.0, 50.0);
+        let term4 = Thermometer::new("6", "Virtual thermometer", -50.0, 50.0);
+        let socket3 = Socket::new("7", "Virtual socket", SocketState::On, 1000.0);
+        let socket4 = Socket::new("8", "Virtual socket", SocketState::Off, 1000.0);
+        room1.add_device("1", Box::<Thermometer>::new(term1));
+        room1.add_device("2", Box::<Thermometer>::new(term2));
+        room1.add_device("3", Box::<Socket>::new(socket1));
+        room1.add_device("4", Box::<Socket>::new(socket2));
+        let mut room2 = Room::new("2", "Hall", HashMap::<String, Box<dyn Device>>::new());
+        
+        room2.add_device("1", Box::<Thermometer>::new(term3));
+        room2.add_device("2", Box::<Thermometer>::new(term4));
+        room2.add_device("3", Box::<Socket>::new(socket3));
+        room2.add_device("4", Box::<Socket>::new(socket4));
+        
+        let mut home = Home::new("1", "My home", HashMap::<String, Box::<Room>>::new());
+        home.add_room("1", Box::<Room>::new(room1));
+        home.add_room("2", Box::<Room>::new(room2));
+        let room = match home.get_mut_room("1") {
+            Some(room) => room,
+            None => panic!("Room was not found"),
+        };
+        let device = match room.get_mut_device("3") 
+        {
+            Some(device) => device,
+            None => panic!("Room wasd not found"),
+        };
         device.update();
         println!("{}", device);
         match device.get_state() {
@@ -388,42 +420,21 @@ mod tests {
 
     #[test]
     fn can_turn_off_socket_through_home_mut_refs() {
-        let mut kitchen_devices = [Device::SocketDevice(Socket::new(
-            1,
-            "coffee",
-            SocketState::On,
-            800.0,
-        ))];
-        let mut rooms = [Room::new(1, "kitchen", &mut kitchen_devices)];
-        let mut home = Home::new(1, "home", &mut rooms);
-
-        home.get_mut_room(0)
-            .get_mut_device(0)
+        let socket1 = Socket::new("1", "Coffee mashine power socket", SocketState::On, 1000.0);
+        let mut room1 = Room::new("1", "Kitchen", HashMap::<String, Box<dyn Device>>::new());
+        room1.add_device("1",Box::<Socket>::new(socket1));
+        let mut home = Home::new("1", "My home", HashMap::<String, Box::<Room>>::new());
+        home.add_room("1", Box::<Room>::new(room1));
+        home.get_mut_room("1").expect("Room not found")
+            .get_mut_device("1").expect("Device not found")
             .set_state(SocketState::Off);
 
-        let socket_after = home.get_mut_room(0).get_mut_device(0);
+        let socket_after = home.get_mut_room("1").expect("Room not found")
+                               .get_mut_device("1").expect("Device non found");
         match socket_after.get_state() {
             Some(state) => assert!(state == SocketState::Off),
             _ => panic!("Device is not a socket"),
         }
     }
 
-    #[test]
-    #[should_panic]
-    fn test_panic_out_of_bound() {
-        let term1 = Thermometer::new(1, "Virtual thermometer", -50.0, 50.0);
-        let term2 = Thermometer::new(2, "Virtual thermometer", -50.0, 50.0);
-        let socket1 = Socket::new(1, "Virtual socket", SocketState::On, 1000.0);
-        let socket2 = Socket::new(2, "Virtual socket", SocketState::Off, 1000.0);
-
-        let mut devices = [
-            Device::ThermometerDevice(term1),
-            Device::ThermometerDevice(term2),
-            Device::SocketDevice(socket1),
-            Device::SocketDevice(socket2),
-        ];
-        let room = Room::new(1, "Kitchen", &mut devices);
-
-        room.get_device(99);
-    }
 }
